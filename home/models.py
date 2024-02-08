0164564fa5745f5771405f4b931bb19db8b99608
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2024-02-08 12:46:22 +0200

    Added the hero image stuff

--- home/models.py
+++ home/models.py
@@ -5,6 +5,27 @@ from wagtail.models import Page
 
 class HomePage(Page):
     #TODO: Big Hero Image 1
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        #So that if there's no image no erros are thrown
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        help_text="This is here as the Main image on your home page. The first one"
+    )
+
+    main_image_text = models.Charfield(
+        max_length=255,
+        help_text="This is your main image text like your Title and name."
+    )
+    main_image_subText = models.Charfield(
+        max_length = 255,
+        help_text= "this is the samll text you will have under your main Image text"
+    )
+
+    
+
     #TODO: Hero Message
 
     #TODO: Call to Action 1
