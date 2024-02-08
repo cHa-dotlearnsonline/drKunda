2eb6e82a71fee442ea57ec589dc3ea8f7dee153a
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2024-02-08 12:13:04 +0200

    Added my TODOs in the back end for the homepage

--- home/models.py
+++ home/models.py
@@ -4,4 +4,20 @@ from wagtail.models import Page
 
 
 class HomePage(Page):
+    #TODO: Big Hero Image 1
+    #TODO: Hero Message
+
+    #TODO: Call to Action 1
+    #TODO: Call to ACtion Image
+
+    #TODO: Story Title 1
+    #TODO: Story Text
+
+    #TODO: CAll to Action 2 Huge
+    #TODO: call to ACtion 2 button
+
+    #TODO: Customer Review Section (Make streamfield)
+
+    #TODO: Social Media Links and Images
+
     pass
