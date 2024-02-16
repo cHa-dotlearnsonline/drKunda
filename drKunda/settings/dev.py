68bb66b9942cc7e663323d3128a2821df03a683b
cHa-dotlearnsonline <chawalearnsonline@gmail.com>
2024-02-16 20:21:45 +0200

    made sure to set things up for production deployment

--- drKunda/settings/dev.py
+++ drKunda/settings/dev.py
@@ -5,7 +5,7 @@ DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-labn(gcq4@&)-ar%xq_8oh4fk)^z6l64y#cyr&eq2#rlh_a6n3"
-
+# SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
--- drKunda/settings/production.py
+++ drKunda/settings/production.py
@@ -1,7 +1,10 @@
 from .base import *
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 DEBUG = False
 
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 try:
     from .local import *
 except ImportError:
